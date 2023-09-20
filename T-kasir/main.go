8e401c61878856e92472ad43ecbdc4ce9c0dbfed
Reza Hidayat <97022190+rezapace@users.noreply.github.com>
2023-09-20 19:19:24 +0700

    tugas

--- T-kasir/main.go
+++ T-kasir/main.go
@@ -2,68 +2,118 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
+type Barang struct {
+	Nama   string
+	Harga  int
+}
+
+var daftarBarang []Barang
+
 func main() {
-	// bikin 3 inputan : bilangan 1, bilangan 2, operasi / aksi
-	// pencabangan apakah operasi / aksi berupa penjumlahan/pembagian/pengurangan/sisa hasil (modulus)
-	// tampilkan hasilnya
-	var inputBilangan1, inputBilangan2, inputOperasi string
+	for {
+		tampilkanMenuUtama()
 
-	fmt.Println("Silakan masukkan bilangan 1: ")
-	fmt.Scanln(&inputBilangan1)
+		var pilihan string
+		fmt.Scanln(&pilihan)
 
-	bilangan1, err := strconv.Atoi(inputBilangan1)
+		switch pilihan {
+		case "1":
+			inputDataBarang()
+		case "2":
+			beliBarang()
+		case "3":
+			return
+		default:
+			fmt.Println("Pilihan tidak valid.")
+		}
+	}
+}
+
+func tampilkanMenuUtama() {
+	fmt.Println("\nMenu Utama:")
+	fmt.Println("1. Input Data Barang")
+	fmt.Println("2. Beli Barang")
+	fmt.Println("3. Keluar")
+	fmt.Print("Masukkan pilihan: ")
+}
+
+func inputDataBarang() {
+	var nama string
+	var harga string
+
+	fmt.Print("Masukkan nama barang: ")
+	fmt.Scanln(&nama)
+
+	fmt.Print("Masukkan harga barang: ")
+	fmt.Scanln(&harga)
+
+	hargaInt, err := strconv.Atoi(harga)
 	if err != nil {
-		fmt.Println("masukkan hanya integer / angka saja")
+		fmt.Println("Harga harus berupa angka.")
 		return
 	}
 
-	fmt.Println("Silakan masukkan bilangan 2: ")
-	fmt.Scanln(&inputBilangan2)
+	daftarBarang = append(daftarBarang, Barang{nama, hargaInt})
+	fmt.Println("Barang berhasil ditambahkan!")
+}
 
-	bilangan2, err := strconv.Atoi(inputBilangan2)
-	if err != nil {
-		fmt.Println("masukkan hanya integer / angka saja")
+func beliBarang() {
+	tampilkanDaftarBarang()
+
+	var kodeBarang, jumlah string
+	fmt.Println("Masukkan kode barang (0-", len(daftarBarang)-1, "): ")
+	fmt.Scanln(&kodeBarang)
+
+	index, err := strconv.Atoi(kodeBarang)
+	if err != nil || index < 0 || index >= len(daftarBarang) {
+		fmt.Println("Kode barang tidak valid.")
 		return
 	}
 
-	fmt.Println(`
-	Operasi / Aksi :
-	1. Penjumlahan
-	2. Pengurangan
-	3. Pembagian
-	4. Perkalian
-	5. Modulus
-	`)
+	fmt.Println("Masukkan jumlah yang dibeli: ")
+	fmt.Scanln(&jumlah)
 
-	fmt.Println("Silakan masukkan aksi (1-5)")
-	fmt.Scanln(&inputOperasi)
-
-	operasi, err := strconv.Atoi(inputOperasi)
-
-	if err != nil {
-		fmt.Println("masukkan hanya integer / angka saja")
+	qty, err := strconv.Atoi(jumlah)
+	if err != nil || qty <= 0 {
+		fmt.Println("Jumlah tidak valid.")
 		return
 	}
 
-	var hasil int
+	total := daftarBarang[index].Harga * qty
+	fmt.Println("Total belanjaan Anda adalah: ", total)
 
-	if operasi == 1 {
-		hasil = bilangan1 + bilangan2
-	} else if operasi == 2 {
-		hasil = bilangan1 - bilangan2
-	} else if operasi == 3 {
-		hasil = bilangan1 / bilangan2
-	} else if operasi == 4 {
-		hasil = bilangan1 * bilangan2
-	} else if operasi == 5 {
-		hasil = bilangan1 % bilangan2
+	cetakInvoice(daftarBarang[index], qty, total)
+}
+
+func tampilkanDaftarBarang() {
+	fmt.Println("\nDaftar Barang:")
+	for i, b := range daftarBarang {
+		fmt.Printf("%d. %s - Rp%d\n", i, b.Nama, b.Harga)
+	}
+}
+
+func cetakInvoice(barang Barang, jumlah, total int) {
+	invoice := fmt.Sprintf(
+		"Nama Barang: %s\nHarga Satuan: Rp%d\nJumlah: %d\nTotal: Rp%d\n",
+		barang.Nama, barang.Harga, jumlah, total,
+	)
+
+	file, err := os.Create("invoice.txt")
+	if err != nil {
+		fmt.Println("Gagal mencetak invoice: ", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(invoice)
+	if err != nil {
+		fmt.Println("Gagal menulis ke file: ", err)
+		return
 	}
 
-	fmt.Println("Bilangan 1 : ", bilangan1)
-	fmt.Println("Bilangan 2 : ", bilangan2)
-	fmt.Println("Aksi/Operasi  : ", inputOperasi)
-	fmt.Println("Hasil dari Operasi tsb adalah : ", hasil)
+	fmt.Println("Invoice telah dicetak ke file invoice.txt")
 }
